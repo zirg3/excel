c5f249f91ee8ec58111995c08d00874fd98a3143
zirg3 <zirg31@gamil.com>
2021-01-25 16:19:22 +0300

    Finish resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -39,6 +39,29 @@ class Dom {
 
     return this
   }
+
+  get data() {
+    return this.$el.dataset
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    Object.keys(styles).forEach(key => {
+      this.$el.style[key] = styles[key]
+    })
+    // return this.$el.style
+  }
 }
 
 export function $(selector) {
